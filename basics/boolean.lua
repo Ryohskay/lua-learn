75f3b9f850e4c879b21615017f050306ccfdfb67
Ryohskay on Pop <learnerryo@gmail.com>
2023-02-13 22:35:16 +0000

    added notes on boolean and if statements

---
+++ basics/boolean.lua
@@ -0,0 +1,20 @@
+-- Boolean example
+
+print(4 and 5)  --> 5
+print(nil and 13)  --> nil
+
+
+-- boolean in Lua:
+-- In Lua, if statements consider `nil` and `false` as "false", and anything else as "true".
+a = 0
+if a then  -- passes when a = 0
+    print("Hey")
+end --if
+
+
+-- In Lua, only `nil` and `false` are "false".
+if b then -- does not pass bcz b is nil
+    print("Oops!")  -- this is printed
+else
+    print("Oh là là!")  -- this is printed
+end
